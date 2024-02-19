9fa13b3cecb4f0f80be682a0848317e4e5b2cae1
albanbin <alban.combaud@epitech.eu>
2024-02-19 20:23:52 +0100

    add resize handle, not perfect small change everytime

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import LayoutClickable from "@/components/layout/clickable";
 export default function Home() {
   return (
     <LayoutClickable>
-      <h1>Click anywhere to comment</h1>
+      <h1 className="text-4xl font-bold py-12 text-center">
+        Click anywhere to comment 😁
+      </h1>
     </LayoutClickable>
   );
 }
--- components/layout/clickable.tsx
+++ components/layout/clickable.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { ThreadContext } from "@/hooks/threadContext";
 import ThreadButton from "../thread/button";
@@ -11,11 +11,39 @@ export default function LayoutClickable({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { setCurrentThread, threads } = useContext(ThreadContext);
+  const { setThreads, setCurrentThread, threads } = useContext(ThreadContext);
 
   const onClickLayout = (e: React.MouseEvent) => {
     setCurrentThread({ x: e.clientX, y: e.clientY + 20, index: null });
   };
+
+  const [dimensions, setDimensions] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      const newHeight = window.innerHeight;
+      const widthRatio = newWidth / dimensions.width;
+      const heightRatio = newHeight / dimensions.height;
+
+      setThreads(
+        threads.map((thread) => ({
+          ...thread,
+          x: thread.x * widthRatio,
+          y: thread.y * heightRatio,
+        }))
+      );
+
+      setDimensions({ width: newWidth, height: newHeight });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [threads]);
 
   return (
     <>
